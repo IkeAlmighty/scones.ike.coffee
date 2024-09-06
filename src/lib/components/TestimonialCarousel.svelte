d4fac60c54659a2bf6c63baf158ca1d5f5e7b5dd
ikealmighty <isaacyates7@gmail.com>
2024-09-06 07:51:18 -0500

    added testimonials carousel

---
+++ src/lib/components/TestimonialCarousel.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let testimonials = [];
+	let current = 0;
+
+	function incrTestimonial() {
+		if (current + 1 == testimonials.length) {
+			current = 0;
+		} else {
+			current += 1;
+		}
+	}
+
+	function decrTestimonial() {
+		if (current - 1 < 0) {
+			current = testimonials.length - 1;
+		} else {
+			current -= 1;
+		}
+	}
+</script>
+
+<h3>Testimonials</h3>
+<div id="container">
+	{#if testimonials.length > 1}
+		<div id="left">
+			<button on:click={incrTestimonial}>&larr; previous</button>
+		</div>
+	{/if}
+
+	<div id="center">"{testimonials[current].text}"<br /> - {testimonials[current].author}</div>
+
+	{#if testimonials.length > 1}
+		<div id="right">
+			<button on:click={decrTestimonial}>next &rarr;</button>
+		</div>
+	{/if}
+</div>
+
+<style>
+	#left {
+		text-align: left;
+		font-size: small;
+	}
+
+	#right {
+		text-align: right;
+		font-size: small;
+	}
+	#center {
+		margin: 1rem 0;
+	}
+
+	button {
+		border: none;
+		background-color: inherit;
+		text-decoration: underline;
+	}
+</style>
