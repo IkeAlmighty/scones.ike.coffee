925fa2109a66c155c57508c3e3003aaa7ee25670
ikealmighty <isaacyates7@gmail.com>
2024-09-28 15:09:45 -0500

    price change to compensate for different scone sizes

--- src/lib/products.js
+++ src/lib/products.js
@@ -8,21 +8,21 @@ export const products = {
         imageUrl: '/scone_16count.jpg',
         amount: 0,
         details: 'Chocolate chips and blueberries baked into delicious scones.',
-        suggestedPrice: 3.5
+        suggestedPrice: 2.75
     },
     'blueberry-lemon-scone': {
         name: 'Blueberry Lemon Scone',
         imageUrl: '/lemon-blueberry.jpg',
         amount: 0,
         details: 'Lemon zest and blueberries baked into delicious scones.',
-        suggestedPrice: 3.5
+        suggestedPrice: 2.75
     },
     'earl-grey-scone': {
         name: 'Earl Grey Scone',
         imageUrl: '',
         amount: 0,
         details: 'Earl Grey Tea from Anahata Herbals in Duluth, baked into delicious scones with an Earl Grey glaze on top.',
-        suggestedPrice: 3.5
+        suggestedPrice: 2.75
     },
     'delivery-fee': {
         name: 'Delivery Fee',
