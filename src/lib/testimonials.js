484a48fd02fd080f947a15d7e1a4f261566ec2a0
ikealmighty <isaacyates7@gmail.com>
2024-11-03 10:44:07 -0600

    added Bronwyn's testimonial

---
+++ src/lib/testimonials.js
@@ -0,0 +1,24 @@
+const testimonials = [
+    {
+        author: 'Karen Ives',
+        text: 'Scones were awesome, folks like they are "moist" and not dried out like so many scones, mostly gone by the time I left.'
+    },
+    {
+        author: 'Slow Down',
+        text: "Ike makes the best scones I've ever had. Wow are they yummy! Also love Ike's warm and fun spirit - tops off the experience!"
+    },
+    {
+        author: 'Lisa',
+        text: 'The scones were so good! They were fluffy and flavorful. I had a warm blueberry chocolate scone and the flavor combo was super good.'
+    },
+    {
+        author: 'Julia',
+        text: "fuck I'm gonna eat all 4 today :'D :'D ... Oh shit they're delicious"
+    },
+    {
+        author: 'Bronwyn',
+        text: "That was literally the best scone I've ever had wtf",
+    }
+];
+
+export default testimonials;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,25 +15,7 @@
 	// on the backend to the prices listed in the structure:
 	import { products } from '$lib/products.js';
 	import DateSelector from '$lib/components/DateSelector.svelte';
-
-	const testimonials = [
-		{
-			author: 'Karen Ives',
-			text: 'Scones were awesome, folks like they are "moist" and not dried out like so many scones, mostly gone by the time I left.'
-		},
-		{
-			author: 'Slow Down',
-			text: "Ike makes the best scones I've ever had. Wow are they yummy! Also love Ike's warm and fun spirit - tops off the experience!"
-		},
-		{
-			author: 'Lisa',
-			text: 'The scones were so good! They were fluffy and flavorful. I had a warm blueberry chocolate scone and the flavor combo was super good.'
-		},
-		{
-			author: 'Julia',
-			text: "fuck I'm gonna eat all 4 today :'D :'D ... Oh shit they're delicious"
-		}
-	];
+	import testimonials from '$lib/testimonials.js';
 
 	$: suggestedPayment = calcSuggestedPayment(products);
 
