e785923845775e6883062c2247a8c1b3360b21cf
ikealmighty <isaacyates7@gmail.com>
2025-06-22 19:10:26 -0500

    replaced bcrypt with bcryptjs

--- src/lib/server/models/User.js
+++ src/lib/server/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { ADMIN_NUMBER } from '$env/static/private';
 
 const userSchema = new mongoose.Schema(
