535386ee182f2f9dacc104a16f4aa7e67d2dcee8
ikealmighty <isaacyates7@gmail.com>
2025-06-30 16:46:41 -0500

    created dedicated endpoint for reading sms from a single user

---
+++ src/routes/api/twilio/read-sms/+server.js
@@ -0,0 +1,17 @@
+import { ADMIN_NUMBER } from '$env/static/private';
+import Message from '$lib/server/models/Message';
+import { connectToDatabase } from '$lib/server/utils/mongoose';
+import { json } from '@sveltejs/kit';
+import { formatPhoneNumber } from '$lib/server/utils/formatting.js';
+
+export const GET = async ({ url }) => {
+	await connectToDatabase();
+
+	const phoneNumber = formatPhoneNumber(url.searchParams.get('phoneNumber'));
+
+	const messages = await Message.find({ $or: [{ to: phoneNumber }, { from: phoneNumber }] }).lean();
+
+	return json({
+		messages
+	});
+};
