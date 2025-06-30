7d896e9251f7de19d1c7036734d219161d9b9abf
ikealmighty <isaacyates7@gmail.com>
2025-06-30 16:48:19 -0500

    added code that pings admin when a message is stored in the database

--- src/routes/api/twilio/incoming/+server.js
+++ src/routes/api/twilio/incoming/+server.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import Message from '$lib/server/models/Message.js';
 import { xml } from '@sveltejs/kit'; // for TwiML response
 import { connectToDatabase } from '$lib/server/utils/mongoose.js';
+import { ADMIN_NUMBER } from '$env/static/private';
+import { User } from '$lib/server/models/User.js';
 
 export async function POST({ request }) {
 	await connectToDatabase();
@@ -20,7 +22,15 @@ export async function POST({ request }) {
 			direction: 'inbound'
 		});
 
-		console.log(`✅ Stored inbound SMS from ${from}: ${body}`);
+		console.log(`✅ Stored inbound SMS from ${from}`);
+
+		// notify me:
+		const user = await User.find({ phone: from });
+		const message = await client.messages.create({
+			body: `${user ? user.username : from} sent a message. https://scones.ike.coffee/account/admin`,
+			from: TWILIO_NUMBER,
+			to: ADMIN_NUMBER
+		});
 
 		// Respond with empty TwiML (Twilio requires valid response)
 		return new Response(`<?xml version="1.0" encoding="UTF-8"?><Response></Response>`, {
