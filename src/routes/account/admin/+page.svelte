2be0161407907a1ca5883a8f222e757ea9219103
ikealmighty <isaacyates7@gmail.com>
2025-07-02 16:13:38 -0500

    added SenAllMessageComposer to bottom of page

--- src/routes/account/admin/+page.svelte
+++ src/routes/account/admin/+page.svelte
@@ -1,9 +1,16 @@
 <script>
-	import MessageComposer from '$lib/components/MessageComposer.svelte';
+	import MessageComposer from '$lib/components/SendAllMessageComposer.svelte';
 	import MessageLog from '$lib/components/MessageLog.svelte';
 	import MessagingComponent from '$lib/components/MessagingComponent.svelte';
+	import SendAllMessageComposer from '$lib/components/SendAllMessageComposer.svelte';
 </script>
 
 <div class="page-container">
-	<MessagingComponent />
+	<div>
+		<MessagingComponent />
+	</div>
+	<hr />
+	<div>
+		<SendAllMessageComposer />
+	</div>
 </div>
