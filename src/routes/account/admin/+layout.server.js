1ab4a8be53ad5ed9d41452aa4e2c2b0674795b34
ikealmighty <isaacyates7@gmail.com>
2025-08-26 21:39:25 -0500

    removed console log

--- src/routes/account/admin/+layout.server.js
+++ src/routes/account/admin/+layout.server.js
@@ -1,7 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals }) {
-	console.log(locals.user);
 	if (!locals.user || !locals.user.isAdmin) {
 		throw redirect(303, '/login');
 	}
